337c8ac4ad761ce2c8f9dd9c5b0b52524bc9ea13
Codnixus <27430075-codnixus@users.noreply.gitlab.com>
2026-06-27 11:49:07 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-capsule.url = "gitlab:codnixus/nix-capsule?ref=v0.8.0";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        {
+          system,
+          ...
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.nix-capsule.overlays.default
+            ];
+          };
+          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+        in
+        {
+          apps.default = capsule-lib.app;
+          devShells = {
+            default = capsule-lib.mkShell {
+              image = "alpine:latest";
+              devShell = "container";
+              socketPath = "/tmp/personal-website/ncap-socket";
+              containerName = "personal-website";
+              extraOptions = [
+                "-e"
+                "PNPM_HOME"
+                "-p"
+                "4321:4321"
+                "--pid=host"
+              ];
+              wrappers = [
+                "npm"
+                "pnpm"
+                {
+                  name = "astro-ls";
+                  value = "node_modules/.bin/astro-ls";
+                }
+                {
+                  name = "typescript-language-server";
+                  value = "node_modules/.bin/typescript-language-server";
+                }
+                {
+                  name = "prettier";
+                  value = "pnpm exec prettier";
+                }
+              ];
+              preShellHook = ''
+                export PNPM_HOME=''${PNPM_HOME:-$HOME/.local/share/pnpm}
+                mkdir -p "$PNPM_HOME"
+              '';
+            };
+
+            container =
+              pkgs.mkShellNoCC {
+                packages = with pkgs; [
+                  nodejs
+                  pnpm
+                ];
+              };
+          };
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
+}
+
